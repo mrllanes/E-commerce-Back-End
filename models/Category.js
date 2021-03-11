eadd13d601647d72224eed016ea72ec3b14ff227
Mr. Llanes <llanesm@gmail.com>
2021-03-11 02:20:03 -0600

    Modification of routes has started.

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,31 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
+	{
+		// define columns
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		category_name: {
+			// Not sure about this part
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "category",
+	}
 );
 
 module.exports = Category;
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,42 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get("/", async (req, res) => {
+	// find all categories
+	// be sure to include its associated Products
+	try {
+		const categoryData = await Category.findAll();
+		res.status(200).json(categoryData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", async (req, res) => {
+	// find one category by its `id` value
+	// be sure to include its associated Products
+	try {
+		const categoryData = await Category.findByPk(req.params.id, {
+      include: // FINISH HERRRRRRRRREEEEEEEEEEEEEEEEEEEEEEEEEEE
+    });
+		res.status(200).json(categoryData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post("/", async (req, res) => {
+	// create a new category
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", async (req, res) => {
+	// update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete("/:id", async (req, res) => {
+	// delete a category by its `id` value
 });
 
 module.exports = router;
